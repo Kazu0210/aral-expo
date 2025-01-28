c1489e95189656cf324423bff626a3f9d65327a9
Kazu0210 <clffrdkyl@gmail.com>
2025-01-29 07:03:40 +0800

    Added navbar

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -2,16 +2,64 @@ import { Link } from 'expo-router';
 import { View, Text, StyleSheet, TouchableOpacity, StatusBar, registerCallableModule } from 'react-native';
 import { SQLiteProvider, useSQLiteContext } from 'expo-sqlite';
 import styles from '../../assets/styles/home';
+import { Image, ScrollView } from 'react-native';
+import { useState } from 'react';
+
 
 export default function app() {
-    return (
-        <View style={styles.temp_layout}>
-            <StatusBar hidden={false} barStyle="dark-content" />
-            <SQLiteProvider databaseName="moonleaf_db.db" assetSource={{ assetId: require('../../assets/moonleaf_db.db') }}>
-                <View>
+    const [isPressed, setIsPressed] = useState(false);
+    const handlePressIn = () => setIsPressed(true);
+    const handlePressOut = () => setIsPressed(false);
 
-                </View>
-            </SQLiteProvider>
-        </View>
+    return (
+        <ScrollView>
+            <View style={styles.temp_layout}>
+                <StatusBar hidden={false} barStyle="dark-content" />
+                <SQLiteProvider databaseName="moonleaf_db.db" assetSource={{ assetId: require('../../assets/moonleaf_db.db') }}>
+                    <View style={styles.navBar}>
+                        <View> {/* logo container */}
+                            <Image 
+                                source={require('../../assets/images/moonleaf-logo.png')}
+                                style={{ width: 200, height: 100, objectFit: 'contain' }}
+                                />
+                        </View>
+
+                        <Link href="/(home)/viewData">
+                            <TouchableOpacity 
+                                    style={[styles.navBarItem, isPressed && styles.navBarItemPressed]} 
+                                    onPressIn={handlePressIn} 
+                                    onPressOut={handlePressOut}
+                                >
+                                <Image
+                                    source={require('../../assets/images/nav-icons/home.png')}
+                                    style={{ width: 25, height: 25, objectFit: 'contain', marginLeft: 15 }}
+                                />
+                                <Text style={styles.navText}>Home</Text>
+                            </TouchableOpacity>
+                        </Link>
+
+                        <TouchableOpacity style={styles.navBarItem}>
+                            <Image
+                                source={require('../../assets/images/nav-icons/home.png')}
+                                style={{ width: 25, height: 25, objectFit: 'contain', marginLeft: 15 }}
+                            />
+                            <Text style={styles.navText}>Inventory</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.navBarItem}>
+                            <Image
+                                source={require('../../assets/images/nav-icons/home.png')}
+                                style={{ width: 25, height: 25, objectFit: 'contain', marginLeft: 15 }}
+                            />
+                            <Text style={styles.navText}>Orders</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View>
+                        <Text>This is a text for the main container</Text>
+                    </View>
+                </SQLiteProvider>
+            </View>
+        </ScrollView>
     )
 }
--- assets/styles/home.jsx
+++ assets/styles/home.jsx
@@ -9,8 +9,29 @@ const styles = StyleSheet.create({
     },
     temp_layout: {
         borderWidth: 1,
-        borderColor: 'black'
-    }
+        borderColor: 'black',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    navBar: {
+        backgroundColor: '#fff'
+    },
+    navText: {
+        color: '#000',
+        fontWeight: 'bold',
+        fontSize: 16
+    },  
+    navBarItem: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'start',
+        height: 50,
+        gap: 15
+    },
+      navBarItemPressed: {
+        backgroundColor: 'red', // Color when pressed
+    },
 });
 
 export default styles
